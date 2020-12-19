b79983d72405fb7dbf31474bb3ccf1e0fd31c4e9
Flysky12138 <2797299614@qq.com>
2020-12-19 11:11:17 +0800

    master

---
+++ BDO.cpp
@@ -0,0 +1,651 @@
+﻿#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <thread>
+#include <shlobj.h>
+#include <atlimage.h>
+#include <windows.h>
+#include <GdiPlus.h>
+#pragma comment(lib, "Gdiplus.lib")
+#pragma warning(disable : 4996)
+
+using namespace std;
+using namespace Gdiplus;
+
+string Code[200][5];   //待运行文本
+string KeyCode[10][2] = {
+	{"0","48"},{"1","49"},{"2","50"},{"3","51"},{"4","52"},
+	{"5","53"},{"6","54"},{"7","55"},{"8","56"},{"9","57"},
+};//虚拟键码
+string GameKeyCode[80][2] = {
+	{"ESC","1"},{"1","2"},{"2","3"},{"3","4"},{"4","5"},
+	{"5","6"},{"6","7"},{"7","8"},{"8","9"},{"9","10"},
+	{"0","11"},{"","12"},{"","13"},{"","14"},{"TAB","15"},
+	{"Q","16"},{"W","17"},{"E","18"},{"R","19"},{"T","20"},
+	{"Y","21"},{"U","22"},{"I","23"},{"O","24"},{"P","25"},
+	{"","26"},{"","27"},{"ENTER","28"},{"CTRL","29"},{"A","30"},
+	{"S","31"},{"D","32"},{"F","33"},{"G","34"},{"H","35"},
+	{"J","36"},{"K","37"},{"L","38"},{"","39"},{"","40"},
+	{"~","41"},{"SHIFT","42"},{"","43"},{"Z","44"},{"X","45"},
+	{"C","46"},{"V","47"},{"B","48"},{"N","49"},{"M","50"},
+	{",","51"},{"","52"},{"","53"},{"","54"},{"","55"},
+	{"ALT","56"},{"SPACE","57"},{"","58"},{"F1","59"},{"F2","60"},
+	{"F3","61"},{"F4","62"},{"F5","63"},{"F6","64"},{"F7","65"},
+	{"F8","66"},{"F9","67"},{"F10","68"},{"F11","69"},{"SCRLK","70"},
+};//硬件扫描代码
+HWND exeWhnd = GetConsoleWindow();
+HWND gameWhnd, headWhnd;
+int QuickKey = VK_OEM_3; //	`
+string Title = "BDO - Flysky";
+
+//string to int
+int Getint(string str)
+{
+	return atoi(str.c_str());
+}
+//string to wstring
+wstring Getwstring(string str)
+{
+	std::string temp = str;
+	int len = MultiByteToWideChar(CP_ACP, 0, (LPCSTR)temp.c_str(), -1, NULL, 0);
+	wchar_t* wszUtf8 = new wchar_t[len + 1];
+	memset(wszUtf8, 0, len * 2 + 2);
+	MultiByteToWideChar(CP_ACP, 0, (LPCSTR)temp.c_str(), -1, (LPWSTR)wszUtf8, len);
+	return wszUtf8;
+}
+//返回str在数组中对应的键码值
+int GetCodeNum(string str, string keycode[][2])
+{
+	int i = 0;
+	while (keycode[i])
+	{
+		if (str == keycode[i][0])
+		{
+			return Getint(keycode[i][1]);
+			break;
+		}
+		i++;
+	}
+}
+//返回num在数组中对应的字符串
+string GetCodeStr(int num, string keycode[][2])
+{
+	int i = 0;
+	while (keycode[i])
+	{
+		if (to_string(num) == keycode[i][1])
+		{
+			return keycode[i][0];
+			break;
+		}
+		i++;
+	}
+}
+//模拟键盘
+void K(int key, int delay, int end)
+{
+	cout << "\r\u6309\u952e  [ " << GetCodeStr(key, GameKeyCode) << " ]" << endl;
+	keybd_event(0, key, 0, 0);
+	Sleep(delay);
+	keybd_event(0, key, 2, 0);
+	Sleep(end);
+}
+//模拟键盘
+void K1(int key, int delay, int end)
+{
+	cout << "\r\u6309\u952e  [ " << GetCodeStr(key, KeyCode) << " ]" << endl;
+	keybd_event(key, 0, 0, 0);
+	Sleep(delay);
+	keybd_event(key, 0, 2, 0);
+	Sleep(end);
+}
+//模拟键盘(组合按键)
+void K2(int key1, int key2, int delay, int end)
+{
+	cout << "\r\u6309\u952e  [ " << GetCodeStr(key1, GameKeyCode) << " + " << GetCodeStr(key2, GameKeyCode) << " ]" << endl;
+	keybd_event(0, key1, 0, 0);
+	Sleep(50);
+	keybd_event(0, key2, 0, 0);
+	Sleep(delay);
+	keybd_event(0, key2, 2, 0);
+	Sleep(50);
+	keybd_event(0, key1, 2, 0);
+	Sleep(end);
+}
+//移动鼠标指针
+void MoveMouse(int x, int y)
+{
+	RECT rect = { x, y, x + 1, y + 1 };
+	ClipCursor(&rect);
+	ClipCursor(NULL);
+	Sleep(50);
+}
+//模拟鼠标左击
+void L(int x, int y, int end)
+{
+	cout << "\r\u5de6\u51fb  [ " << x << "," << y << " ]" << endl;
+	MoveMouse(x, y);
+	mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+	Sleep(50);
+	mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+	Sleep(end);
+}
+void L(int delay, int end)
+{
+	cout << "\r\u5de6\u51fb" << endl;
+	mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+	Sleep(delay);
+	mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+	Sleep(end);
+}
+//模拟鼠标右击
+void R(int x, int y, int end)
+{
+	cout << "\r\u53f3\u51fb  [ " << x << "," << y << " ]" << endl;
+	MoveMouse(x, y);
+	mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+	Sleep(50);
+	mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+	Sleep(end);
+}
+void R(int delay, int end)
+{
+	cout << "\r\u53f3\u51fb" << endl;
+	mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+	Sleep(delay);
+	mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+	Sleep(end);
+}
+//模拟鼠标拖动
+void M(int x1, int y1, int x2, int y2, int end)
+{
+	cout << "\r\u79fb\u52a8  [ " << x1 << "," << y1 << " " << x2 << "," << y2 << " ]" << endl;
+	MoveMouse(x1, y1);
+	mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+	Sleep(50);
+	MoveMouse(x2, y2);
+	mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+	Sleep(end);
+}
+//模拟鼠标滚轮
+void W(int x, int y, int z, int end)
+{
+	cout << "\r\u6eda\u8f6e  [ " << x << "," << y << " " << z << " ]" << endl;
+	MoveMouse(x, y);
+	while (z)
+	{
+		mouse_event(MOUSEEVENTF_WHEEL, 0, 0, -WHEEL_DELTA, 0);
+		z--;
+		if (z)
+		{
+			Sleep(50 + rand() % 101); //[50,150]
+		}
+	}
+	Sleep(end);
+}
+//等待时间
+void S(int end)
+{
+	Sleep(end);
+}
+//打开游戏
+void OPEN(int end)
+{
+	while (IsWindowVisible(gameWhnd))
+	{
+		Sleep(5000);
+	}
+	headWhnd = GetForegroundWindow();
+	cout << "\r\u6253\u5f00" << endl;
+	ShowWindow(gameWhnd, 1);
+	SetWindowPos(gameWhnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
+	MoveMouse(960, 540);
+	mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+	if (end < 0)
+	{
+		end = -end;
+		Sleep(100);
+		SetWindowPos(gameWhnd, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
+	}
+	if (!IsWindowVisible(gameWhnd))
+	{
+		exit(0);
+	}
+	Sleep(end);
+}
+//关闭游戏
+void CLOSE()
+{
+	if (IsWindowVisible(gameWhnd))
+	{
+		cout << "\r\u5173\u95ed" << endl;
+		ShowWindow(gameWhnd, 0);
+		SetForegroundWindow(headWhnd);
+	}
+}
+//读取文件并储存于Code
+void ReadFiletoCode(string file)
+{
+	string line, out;
+	ifstream in(file);
+	if (in)
+	{
+		for (int i = 0; i < sizeof(Code) / sizeof(Code[0]); i++)
+		{
+			for (int j = 0; j < sizeof(Code[0]) / sizeof(Code[0][0]); j++)
+			{
+				Code[i][j] = "\0";
+			}
+		}
+		int length = 0;
+		while (getline(in, line))
+		{
+			int y = 0;
+			istringstream str(line);
+			while (str >> out)
+			{
+				Code[length][y] = out;
+				y++;
+			}
+			length++;
+		}
+	}
+}
+//输出字的颜色
+void CoutColor(int tag) {
+	switch (tag)
+	{
+	case 0:
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY);
+		break;
+	case 111:
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+		break;
+	case 100:
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_RED);
+	}
+}
+//倒计时
+void ShowTime(int delay)
+{
+	while (delay != 0)
+	{
+		cout << "\r\u5012\u8ba1\u65f6: ";
+		CoutColor(100);
+		cout << setw(2) << setfill('0') << delay / 60 << "\u5206" << setw(2) << setfill('0') << delay % 60 << "\u79d2 ";
+		CoutColor(111);
+		if (delay <= 3 && !IsWindowVisible(gameWhnd))
+		{
+			HWND sqa = GetForegroundWindow();
+			if (exeWhnd != sqa)
+			{
+				ShowWindow(exeWhnd, 1);
+				SetWindowPos(exeWhnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
+				SetForegroundWindow(sqa);
+			}
+			if (delay == 1) ShowWindow(exeWhnd, SW_SHOWMINIMIZED);
+		}
+		Sleep(1000);
+		delay--;
+	}
+}
+//设置热键
+void SetHotKey(int key, string tag)
+{
+	if (tag == "start")
+	{
+		while (true)
+		{
+			if (key > 0)
+			{
+				if (GetAsyncKeyState(key) & 0x8000)
+				{
+					break;
+				}
+			}
+			else
+			{
+				if (GetAsyncKeyState(-key) & 0x8000)
+				{
+					while (GetAsyncKeyState(-key) & 0x8000)
+					{
+						Sleep(1);
+					}
+					break;
+				}
+			}
+			Sleep(1);
+		}
+	}
+	else if (tag == "stop")
+	{
+		while (true)
+		{
+			if (GetAsyncKeyState(VK_END) & 0x8000)
+			{
+				exit(0);
+			}
+			Sleep(1);
+		}
+	}
+}
+//设置窗口标题
+void SetTitle(string path)
+{
+	string str1 = path.substr(path.find_last_of('\\') + 1);
+	string str2 = str1.substr(0, str1.find_last_of('.'));
+	LPCSTR title = (LPCSTR)str2.c_str();
+	SetConsoleTitleA(title);
+}
+//设置窗口样式
+void SetStyle()
+{
+	SMALL_RECT rc = { 0, 0, 30, 20 };
+	SetConsoleWindowInfo(GetStdHandle(STD_OUTPUT_HANDLE), TRUE, &rc);
+	SetWindowLongPtrA(
+		exeWhnd,
+		GWL_STYLE,
+		GetWindowLongPtrA(exeWhnd, GWL_STYLE) & ~WS_SIZEBOX & ~WS_MAXIMIZEBOX);
+	ShowWindow(exeWhnd, SW_SHOWMINIMIZED);
+}
+//获取坐标并复制到剪切板
+void ClipBoard(char* str)
+{
+	HWND hWnd = NULL;
+	OpenClipboard(hWnd);
+	EmptyClipboard();
+	HANDLE hHandle = GlobalAlloc(GMEM_FIXED, 1000);
+	char* pData = (char*)GlobalLock(hHandle);
+	strcpy(pData, str);
+	SetClipboardData(CF_TEXT, hHandle);
+	GlobalUnlock(hHandle);
+	CloseClipboard();
+}
+void GetPosition()
+{
+	bool first = true;
+	int hotkey = 1000;
+	RegisterHotKey(NULL, hotkey, MOD_ALT, QuickKey);
+	MSG msg = { 0 };
+	while (GetMessage(&msg, NULL, 0, 0) != 0)
+	{
+		if (msg.wParam == hotkey)
+		{
+			if (first)
+			{
+				system("cls");
+				first = false;
+			}
+			POINT p;
+			GetCursorPos(&p);
+			string str = to_string(p.x) + "," + to_string(p.y);
+			char* position = (char*)str.c_str();
+			cout << position << endl;
+			ClipBoard(position);
+		}
+	}
+}
+void GetPosition(int& x, int& y)
+{
+	int hotkey = 1000;
+	RegisterHotKey(NULL, hotkey, MOD_ALT, QuickKey);
+	MSG msg = { 0 };
+	while (GetMessage(&msg, NULL, 0, 0) != 0)
+	{
+		if (msg.wParam == hotkey)
+		{
+			POINT p;
+			GetCursorPos(&p);
+			x = p.x;
+			y = p.y;
+			break;
+		}
+	}
+}
+//获取文档路径
+string GetDocumentsPath()
+{
+	LPITEMIDLIST pidl;
+	LPMALLOC pShellMalloc;
+	char szDir[200];
+	if (SUCCEEDED(SHGetMalloc(&pShellMalloc)))
+	{
+		if (SUCCEEDED(SHGetSpecialFolderLocation(NULL, CSIDL_PERSONAL, &pidl)))
+		{
+			SHGetPathFromIDListA(pidl, szDir);
+			pShellMalloc->Free(pidl);
+		}
+		pShellMalloc->Release();
+	}
+	return string(szDir);
+}
+//游戏全屏截取保存为BMP
+void ScreenShot()
+{
+	HDC hDCScreen = GetDC(NULL);
+	int nBitPerPixel = GetDeviceCaps(hDCScreen, BITSPIXEL);
+	int nWidthScreen = GetDeviceCaps(hDCScreen, HORZRES);
+	int nHeightScreen = GetDeviceCaps(hDCScreen, VERTRES);
+	CImage m_MyImage;
+	m_MyImage.Create(nWidthScreen, nHeightScreen, nBitPerPixel);
+	HDC hDCImg = m_MyImage.GetDC();
+	BitBlt(hDCImg, 0, 0, nWidthScreen, nHeightScreen, hDCScreen, 0, 0, SRCCOPY);
+	CString strFileName = GetDocumentsPath().c_str();
+	strFileName += _T("\\Black Desert\\ScreenShot\\");
+	CreateDirectory((LPCTSTR)strFileName, NULL);
+	strFileName += _T("BDO.bmp");
+	m_MyImage.Save(strFileName, ImageFormatPNG);
+	m_MyImage.ReleaseDC();
+}
+//根据颜色找到匹配的第一个坐标
+void getColorXY(int red, int green, int blue, int& x, int& y) {
+	ULONG_PTR gdiplusToken;
+	GdiplusStartupInput gdiplusStartupInput;
+	GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+	string imgPath = GetDocumentsPath() + "\\Black Desert\\ScreenShot\\BDO.bmp";
+	Bitmap* image = new Bitmap(Getwstring(imgPath).c_str());
+	int width = image->GetWidth();
+	int height = image->GetHeight();
+	Color color;
+	for (int i = 0; i < width; i++)
+	{
+		for (int j = 0; j < height; j++)
+		{
+			image->GetPixel(i, j, &color);
+			if (red == color.GetRed()) {
+				if (green == color.GetGreen()) {
+					if (blue == color.GetBlue()) {
+						x = i;
+						y = j;
+						return;
+					}
+				}
+			}
+		}
+	}
+	delete image;
+	GdiplusShutdown(gdiplusToken);
+}
+//得到坐标
+void GetXY(string& str, int& x, int& y)
+{
+	//x,y
+	if (str == "x,y")
+	{
+		GetPosition(x, y);
+		str = to_string(x) + ',' + to_string(y);
+		Sleep(1000);
+	}
+	//color(255,255,255)
+	else if (str.substr(0, 5) == "color")
+	{
+		ScreenShot();
+		string rgb;
+		int R = 0, G = 0, B = 0;
+		rgb = str.substr(str.find("(") + 1, str.find_last_of(")") - 6); //r,g,b
+		R = Getint(rgb.substr(0, rgb.find(",")));
+		rgb = rgb.substr(rgb.find(",") + 1); //g,b
+		G = Getint(rgb.substr(0, rgb.find(",")));
+		rgb = rgb.substr(rgb.find(",") + 1); //b
+		B = Getint(rgb);
+		getColorXY(R, G, B, x, y);
+	}
+	//0,0
+	else
+	{
+		size_t division = str.find_last_of(',');
+		x = Getint(str.substr(0, division));
+		y = Getint(str.substr(1 + division));
+	}
+}
+//单行脚本处理
+void RunKey(string str[])
+{
+	if (IsWindowVisible(gameWhnd))
+	{
+		if (str[0] == "K")
+		{
+			K(GetCodeNum(str[1], GameKeyCode), Getint(str[2]), Getint(str[3]));
+		}
+		if (str[0] == "K1")
+		{
+			K1(GetCodeNum(str[1], KeyCode), Getint(str[2]), Getint(str[3]));
+		}
+		if (str[0] == "K2")
+		{
+			K2(GetCodeNum(str[1], GameKeyCode), GetCodeNum(str[2], GameKeyCode), Getint(str[3]), Getint(str[4]));
+		}
+		if (str[0] == "L" || str[0] == "R" || str[0] == "W")
+		{
+			int x, y;
+			bool Sign = false;
+			if (str[1].find(',') != string::npos || str[1].find("color") != string::npos)
+			{
+				GetXY(str[1], x, y);
+				Sign = true;
+			}
+			if (str[0] == "L")
+			{
+				Sign ? L(x, y, Getint(str[2])) : L(Getint(str[1]), Getint(str[2]));
+			}
+			if (str[0] == "R")
+			{
+				Sign ? R(x, y, Getint(str[2])) : R(Getint(str[1]), Getint(str[2]));
+			}
+			if (str[0] == "W")
+			{
+				W(x, y, Getint(str[2]), Getint(str[3]));
+			}
+		}
+		if (str[0] == "M")
+		{
+			int x1, y1, x2, y2;
+			GetXY(str[1], x1, y1);
+			GetXY(str[2], x2, y2);
+			M(x1, y1, x2, y2, Getint(str[3]));
+
+		}
+		if (str[0] == "S")
+		{
+			S(Getint(str[1]));
+		}
+	}
+	if (str[0] == "OPEN")
+	{
+		OPEN(Getint(str[1]));
+	}
+	if (str[0] == "CLOSE")
+	{
+		CLOSE();
+	}
+}
+//FOR循环
+int RunFor(int i, int times)
+{
+	int j;
+	while (times--)
+	{
+		for (j = i + 1; Code[j][0] != "END" && Code[j][0] != "\0"; j++)
+		{
+			if (Code[j][0] == "FOR")
+			{
+				j = RunFor(j, Getint(Code[j][1]));
+			}
+			RunKey(Code[j]);
+		}
+	}
+	return j;
+}
+//脚本整体处理
+void RunCode()
+{
+	if (Code[0][0] == "BDO")
+	{
+		int num = 1, total = Getint(Code[0][1]), HotKey = Getint(Code[0][3]);
+		while (total--)
+		{
+			if (HotKey)
+			{
+				CoutColor(100);
+				cout << "\u542f\u52a8\u70ed\u952e\u952e\u7801  " << HotKey << endl;
+				cout << "\u505c\u6b62\u70ed\u952e\u6309\u952e  END ";
+				SetHotKey(HotKey, "start");
+			}
+			system("cls");
+			CoutColor(111);
+			cout << "\u8fd0\u884c\u6b21\u6570: ";
+			CoutColor(100);
+			cout << num++ << " / " << Getint(Code[0][1]) << endl
+				<< endl;
+			CoutColor(0);
+			gameWhnd = FindWindow("BlackDesertWindowClass", NULL);
+			for (int i = 0; Code[i][0] != "\0"; i++)
+			{
+				if (Code[i][0] == "FOR")
+				{
+					i = RunFor(i, Getint(Code[i][1]));
+				}
+				RunKey(Code[i]);
+			}
+			CoutColor(111);
+			cout << endl;
+			if (total != 0)
+			{
+				ShowTime(Getint(Code[0][2]));
+			}
+		}
+	}
+}
+//主函数
+int main(int argc, char* argv[])
+{
+	switch (argc)
+	{
+	case 1:
+		SetStyle();
+		SetTitle(Title);
+		CoutColor(100);
+		cout << "ALT + ` \u83b7\u53d6\u5750\u6807\u4f4d\u7f6e\u5230\u526a\u5207\u677f " << endl;
+		GetPosition();
+		break;
+	case 2:
+		SetStyle();
+		SetTitle(argv[1]);
+		ReadFiletoCode(argv[1]);
+		Sleep(1000);
+		if (Getint(Code[0][3]))
+		{
+			thread t(SetHotKey, 0, "stop"); t.detach();
+		}
+		RunCode();
+		break;
+	default:
+		SetStyle();
+		SetTitle(Title);
+		ShowWindow(exeWhnd, 1);
+		cout << "QQ: 2797299614" << endl << endl;
+	}
+	if (argc != 2) system("pause");
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,169 @@
+<h2 align="center">运行</h2>
+
+> ### `a.exe`
+>
+> 快捷键 `` ALT+` `` 获取鼠标坐标并复制到剪切板
+
+> ### `a.exe a.txt`
+>
+> 运行脚本 `a.txt`
+
+<br>
+
+<h2 align="center">脚本说明</h2>
+
+1. 头部（必须写且在第一行）
+
+```
+BDO <run_times> <runEnd_wait(s)> [hotkey]
+BDO 3 0 70
+```
+
+2. 打开 / 关闭游戏
+
+```
+OPEN [wait(ms)]
+OPEN 800
+#正数：HWND_TOPMOST => 将窗口置于最上方
+#负数：HWND_BOTTOM  => 将窗口置于最下方
+
+CLOSE
+```
+
+3. 按键
+
+```
+K <key> <keyUp_delay(ms)> [wait(ms)]
+K Q 50 800
+
+K1 <key1> <keyUp_delay(ms)> [wait(ms)]
+K1 1 50 800
+
+K2 <key> <key> <keyUp_delay(ms)> [wait(ms)]
+K2 ALT Q 50 800
+```
+
+4. 鼠标左击
+
+```
+L <keyUp_delay(ms)> [wait(ms)]
+L 50 800
+
+L <x,y>/<color(r,g,b)> [wait(ms)]
+L 0,0 800
+```
+
+```
+<x,y>           =>  0,0 / x,y
+<color(r,g,b)>  =>  color(255,255,255)
+```
+
+5. 鼠标右击
+
+```
+R <keyUp_delay(ms)> [wait(ms)]
+R 50 800
+
+R <x,y>/<color(r,g,b)> [wait(ms)]
+R 0,0 800
+```
+
+6. 鼠标滚轮
+
+```
+W <x,y>/<color(r,g,b)> <downMove_times> [wait(ms)]
+W 0,0 3 800
+```
+
+7. 鼠标左击拖动
+
+```
+M <x,y>/<color(r,g,b)> <x,y>/<color(r,g,b)> [wait(ms)]
+M 0,0 100,100 800
+```
+
+8. 等待时间
+
+```
+S <wait(ms)>
+S 500
+```
+
+-   `run_times`：运行次数
+-   `runEnd_wait(s)`：单次脚本运行完后的等待时间，当倒计时到达 `3` 秒内且游戏最小化时将弹出窗口，`0` 秒时再隐藏
+-   `hotkey`：用于运行脚本的快捷按键，虚拟键码自行搜索，当设置有运行快捷键时按键 `End` 可结束脚本（正数：按键按下就执行，负数：按键抬起才执行）
+-   `wait(ms)`：运行单行命令后的等待时间
+-   `key`：可填按键 `0-9` `A-Z` `F1-F11` `ESC` `TAB` `ENTER` `CTRL` `SHIFT` `ALT` `SPACE` `SCRLK` `~` `,`
+-   `key1`：可填按键 `0-9`（当且仅当选择物品数量时才用此命令）
+-   `keyUp_delay(ms)`：按键按下到抬起之间的时间
+-   `x,y`：可以填写坐标如 `100,200`，也可以直接填写 `x,y`。当填写 `x,y` 时，程序第一次运行到这行代码时，你需要按快捷键 `` ALT+` `` 获取坐标，之后的运行就自动使用这次得到的坐标
+-   `color(r,g,b)`：获取在图片 `BDO.bmp`（自动截取的图片，保存在游戏截图文件夹内）中第一个匹配到的颜色的坐标 （匹配方式：从 `0,0` 点开始竖向匹配）
+-   `downMove_times`：向下移动，以一个背包格子为单位
+
+> 单行命令最后为 `0` 时可不填写  
+> 所有功能均需游戏没最小化才会执行，否则直接跳过  
+> 脚本以空行作为结束标记, 非匹配功能不会执行（例：`#K` 用以注释该命令）
+
+<br>
+
+<h2 align="center">循环语法</h2>
+
+```
+FOR <run_times>
+K/K1/K2/W/L/R/M
+END
+```
+
+```
+FOR 180
+    K R 10 800
+    FOR 180
+        K R 10 800
+        K SPACE 10 40
+        K2 ALT Q 50 800
+    END
+    S 800
+    K R 10
+END
+```
+
+<br>
+
+<h2 align="center">脚本实列</h2>
+
+```
+BDO 100 0 -2 #右键
+K S 10 800
+K R 10 800
+FOR 10
+	L 300 100
+	R 300 100
+END
+```
+
+<br>
+
+<h2 align="center">依赖</h2>
+
+```
+dumpbin.exe /dependents BDO.exe
+
+KERNEL32.dll
+USER32.dll
+GDI32.dll
+SHELL32.dll
+MSVCP140.dll
+SHLWAPI.dll
+gdiplus.dll
+VCRUNTIME140_1.dll
+VCRUNTIME140.dll
+api-ms-win-crt-runtime-l1-1-0.dll
+api-ms-win-crt-string-l1-1-0.dll
+api-ms-win-crt-stdio-l1-1-0.dll
+api-ms-win-crt-heap-l1-1-0.dll
+api-ms-win-crt-utility-l1-1-0.dll
+api-ms-win-crt-filesystem-l1-1-0.dll
+api-ms-win-crt-convert-l1-1-0.dll
+api-ms-win-crt-math-l1-1-0.dll
+api-ms-win-crt-locale-l1-1-0.dll
+```
